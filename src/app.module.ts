d826834c9f65ce8570a3dfc05fe7fa3337d5e1d5
Bruno Martins <bruno.martins.cesfi@gmail.com>
2025-09-05 13:40:31 -0300

    Add authentication

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,24 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
+import { AuthModule } from './auth/auth';
+import { AuthenticationController } from './controllers/authentication';
 import { CreateAccountController } from './controllers/create-account';
 import { envSchema } from './env';
 import { PrismaService } from './prisma/prisma.service';
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    validate: (env) => envSchema.parse(env),
-    isGlobal: true,
-  })],
-  controllers: [CreateAccountController],
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+    AuthModule
+  ],
+  controllers: [
+    CreateAccountController,
+    AuthenticationController
+  ],
   providers: [PrismaService],
 })
 export class AppModule { }
---
+++ src/auth/auth.ts
@@ -0,0 +1,26 @@
+import { Module } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { JwtModule } from "@nestjs/jwt";
+import { PassportModule } from "@nestjs/passport";
+
+import { Env } from "src/env";
+
+@Module({
+    imports: [
+        PassportModule,
+        JwtModule.registerAsync({
+            inject: [ConfigService],
+            useFactory(config: ConfigService<Env, true>) {
+                const privateKey = config.get<string>('JWT_PRIVATE_KEY', { infer: true })
+                const publicKey = config.get<string>('JWT_PUBLIC_KEY', { infer: true })
+                return {
+                    privateKey: Buffer.from(privateKey, 'base64').toString('utf-8'),
+                    publicKey: Buffer.from(publicKey, 'base64').toString('utf-8'),
+                    signOptions: { algorithm: 'RS256' }
+                }
+            }
+        })
+    ],
+    exports: [JwtModule],
+})
+export class AuthModule { }
