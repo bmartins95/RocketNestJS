d74b4b8e07b4709d30ebca9022f5c677fbd25cea
Bruno Martins <bruno.martins.cesfi@gmail.com>
2025-09-12 14:14:21 -0300

    Fix file path import

--- src/infra/auth/jwt-strategy.ts
+++ src/infra/auth/jwt-strategy.ts
@@ -4,7 +4,8 @@ import { PassportStrategy } from "@nestjs/passport";
 import { ExtractJwt, Strategy } from "passport-jwt";
 import z from "zod";
 
-import type { Env } from "@/env";
+import { Env } from "../env";
+
 
 
 const tokenSchema = z.object({
