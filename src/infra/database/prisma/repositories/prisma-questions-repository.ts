3f98e80f0cf040b3d87253dfd08e5d69039ea51d
Bruno Martins <bruno.martins.cesfi@gmail.com>
2025-09-16 18:19:37 -0300

    Finish prisma question repository implementation

--- src/infra/database/prisma/repositories/prisma-questions-repository.ts
+++ src/infra/database/prisma/repositories/prisma-questions-repository.ts
@@ -27,22 +27,56 @@ export class PrismaQuestionsRepository implements QuestionsRepository {
     }
 
     async findBySlug(slug: string): Promise<Question | null> {
-        throw new Error("Method not implemented.");
+        const question = await this.prisma.question.findUnique({
+            where: {
+                slug
+            },
+        });
+
+        if (!question) {
+            return null;
+        }
+
+        return PrismaQuestionMapper.toDomain(question);
     }
 
     async findManyRecent(page: PaginationParams): Promise<Question[]> {
-        throw new Error("Method not implemented.");
+        const questions = await this.prisma.question.findMany({
+            orderBy: {
+                createdAt: 'desc'
+            },
+            take: 20,
+            skip: (page.page - 1) * 20
+        });
+
+        return questions.map(question => {
+            return PrismaQuestionMapper.toDomain(question);
+        });
     }
 
     async save(question: Question): Promise<void> {
-        throw new Error("Method not implemented.");
+        const data = PrismaQuestionMapper.toPrisma(question);
+
+        await this.prisma.question.update({
+            where: {
+                id: question.id.toString(),
+            },
+            data,
+        });
     }
 
     async create(question: Question): Promise<void> {
-        throw new Error("Method not implemented.");
+        const data = PrismaQuestionMapper.toPrisma(question);
+        await this.prisma.question.create({
+            data,
+        })
     }
 
     async delete(question: Question): Promise<void> {
-        throw new Error("Method not implemented.");
+        await this.prisma.question.delete({
+            where: {
+                id: question.id.toString(),
+            }
+        });
     }
 }
